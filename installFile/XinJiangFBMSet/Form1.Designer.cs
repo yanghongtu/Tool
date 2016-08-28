2ab52cdb02dc9826c53078c0218905955c53624e
yanghongtu <121694393@qq.com>
2016-08-28 12:01:27 +0800

    test

--- installFile/XinJiangFBMSet/Form1.Designer.cs
+++ installFile/XinJiangFBMSet/Form1.Designer.cs
@@ -244,7 +244,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(375, 353);
+            this.ClientSize = new System.Drawing.Size(369, 353);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
